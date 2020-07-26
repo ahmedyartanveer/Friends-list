af52158771a351268775768000ed3f19069c92ca
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-07-27 02:48:04 +0500

    PersonList Component

---
+++ src/Components/PersonList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Person from "./Person";
+
+const PersonList = () => {
+  const people = [
+    {
+      img: 11,
+      name: "Ahmed",
+      job: "developer",
+    },
+    {
+      img: 13,
+      name: "Kazim",
+      job: "Engineer",
+    },
+    {
+      img: 12,
+      name: "izhan",
+      job: "Designer",
+    },
+    {
+      img: 14,
+      name: "Qasim",
+      job: "Banker",
+    },
+  ];
+  debugger;
+  return (
+    <div>
+      {/* {people.map((i, j) => (
+        <Person person={people[i]} key={j} />
+      ))} */}
+      <Person person={people[0]} />
+      <Person person={people[1]}>I'm son of Ahmed</Person>
+      <Person person={people[2]} />
+    </div>
+  );
+};
+
+export default PersonList;

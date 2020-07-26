0fb6aa8ef778ff8fb61b7039837085369ddd0c27
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-07-27 02:47:49 +0500

    Added Person Component

---
+++ src/Components/Person.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Person = (props) => {
+  debugger;
+  const { img, name, job } = props.person;
+  const { children } = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+
+  return (
+    <div className="person">
+      <img src={url} alt="/" />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </div>
+  );
+};
+
+export default Person;
